8196900442601a3c3c341a4a540476660cf8d6c6
Giuseppe Davide Salvatore Pomo <giuseppepomo8@gmail.com>
2025-01-08 15:54:36 +0100

    quinto commit

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -6,7 +6,7 @@ const AllTheBooks = () => {
     <div className="d-flex flex-wrap justify-content-center">
       {books.map((book) => (
         <Card key={book.id} className="m-3" style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={book.img} alt={book.title} />
+          <Card.Img variant="top" src={book.img} alt={book.title} style={{height:'400px'}}/>
           <Card.Body>
             <Card.Title>{book.title}</Card.Title>
           </Card.Body>
